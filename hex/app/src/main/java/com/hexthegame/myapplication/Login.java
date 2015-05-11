0d6c3e1c3cefa479ada545f74aff2f06cdf706b5
latertater <cameronswain1@gmail.com>
2015-05-11 08:10:33 -0500

    Revert "Revert "May 7""
    This reverts commit 47c1f82b36617235063303af5f282eb81f470335.

--- hex/app/src/main/java/com/hexthegame/myapplication/Login.java
+++ hex/app/src/main/java/com/hexthegame/myapplication/Login.java
@@ -1,27 +1,34 @@
 package com.hexthegame.myapplication;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.IntentSender;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.plus.Plus;
+import com.google.android.gms.common.SignInButton;
 
 public class Login extends Activity
         implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private static final int RC_SIGN_IN = 0;
     private GoogleApiClient mGoogleApiClient;
+    private boolean mSignInClicked;
     private boolean mIntentInProgress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ((SignInButton)findViewById(R.id.sign_in_button)).setOnClickListener((View.OnClickListener)this);
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addConnectionCallbacks(this)
@@ -31,9 +38,17 @@ public class Login extends Activity
                 .build();
     }
 
+    public void onClick(View view) {
+        if (view.getId() == R.id.sign_in_button && !mGoogleApiClient.isConnecting()) {
+            mSignInClicked = true;
+            mGoogleApiClient.connect();
+        }
+    }
+
     @Override
     public void onConnected(Bundle bundle) {
-
+        mSignInClicked = false;
+        Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
     }
 
     @Override
@@ -43,16 +58,33 @@ public class Login extends Activity
 
     @Override
     public void onConnectionFailed(ConnectionResult result) {
-        if (!mIntentInProgress && result.hasResolution()) {
-            try {
-                mIntentInProgress = true;
-                startIntentSenderForResult(result.getResolution().getIntentSender(),
-                        RC_SIGN_IN, null, 0, 0, 0);
-            } catch (IntentSender.SendIntentException e) {
-                // The intent was canceled before it was sent.  Return to the default
-                // state and attempt to connect to get an updated ConnectionResult.
-                mIntentInProgress = false;
-                mGoogleApiClient.connect();
+        if (!mIntentInProgress) {
+            if(mSignInClicked && result.hasResolution()) {
+                try {
+                    result.startResolutionForResult(this, RC_SIGN_IN);
+                    mIntentInProgress = true;
+                    //startIntentSenderForResult(result.getResolution().getIntentSender(),
+                     //       RC_SIGN_IN, null, 0, 0, 0);
+                } catch (IntentSender.SendIntentException e) {
+                    // The intent was canceled before it was sent.  Return to the default
+                    // state and attempt to connect to get an updated ConnectionResult.
+                    mIntentInProgress = false;
+                    mGoogleApiClient.connect();
+                }
+            }
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
+        if (requestCode == RC_SIGN_IN) {
+            if (responseCode != RESULT_OK) {
+                mSignInClicked = false;
+            }
+
+            mIntentInProgress = false;
+
+            if (!mGoogleApiClient.isConnected()) {
+                mGoogleApiClient.reconnect();
             }
         }
     }
